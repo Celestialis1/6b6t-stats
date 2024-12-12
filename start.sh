51a80f8d838c08c998d66e07d6b4f12479f03812
Celestialis <147392489+Celestialis1@users.noreply.github.com>
2024-12-13 07:43:05 +1100

    Delete start.sh

--- start.sh
+++
@@ -1,6 +0,0 @@
-echo -e "\033]0;6b6t Stat Viewer | Vanguard\007"
-clear
-echo "Setting application title and running installer..."
-cd "$(dirname "$0") || exit 1"
-node installer.js && echo "Installer finished successfully."
-read -p "Press Enter to exit..." 
